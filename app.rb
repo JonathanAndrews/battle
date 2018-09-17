17ad932154900bfca6b1c8ce57976405247b7ccc
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-17 17:36:14 +0000

    redirecting to /play

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get "/" do
+  enable :sessions
+
+  get "/name" do
     erb(:index)
   end
 
+  post "/name" do
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect "/play"
+  end
+
+  get "/play" do
+    p session[:player_1]
+    p session[:player_2]
+    erb(:play)
+  end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,16 +1,12 @@
 require 'capybara/rspec'
 
 feature "Testing infrastructure" do
-  scenario "Can run app and check page content" do
-    visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
-  end
 
   scenario "can fill in form and submit name" do
-    visit("/")
-    fill_in("First Name", with: "Oscar")
-    fill_in("Last Name", with: "SPAGHETTI AND MEATBALLS")
-    click_button("Submit")
+    visit("/name")
+    fill_in(:player_1, with: "Oscar")
+    fill_in(:player_2, with: "SPAGHETTI AND MEATBALLS")
+    click_button 'Submit'
     expect(page).to have_content("Oscar SPAGHETTI AND MEATBALLS")
   end
 end
